9cd041a6d1477d5eea256bd6d8dbffb6fe5ebb3c
yuanxuanS <panpanono@163.com>
2024-03-23 11:52:20 +0800

    修改policy类指向同一个：复制参数

--- psro_test/init_policy.py
+++ psro_test/init_policy.py
@@ -20,6 +20,7 @@ from rl4co.utils import RL4COTrainer
 from rl4co import utils
 from typing import List, Optional, Tuple
 import pyrootutils
+import random
 root = pyrootutils.setup_root(__file__, dotenv=True, pythonpath=True) # 把当下project路径放到环境变量里面。
 log = utils.get_pylogger(__name__)
 
@@ -86,7 +87,7 @@ def nash_solver(payoff):
 
     # Lemke-Howson couldn't find equilibrium OR
     # Lemke-Howson return error - game may be degenerate. try other approaches
-    if equilibrium is None or (equilibrium[0].shape != (payoff.shape[0],) and equilibrium[1].shape != (payoffs.shape[0],)):
+    if equilibrium is None or (equilibrium[0].shape != (payoff.shape[0],) and equilibrium[1].shape != (payoff.shape[0],)):
         # try other
         print('\n\n\n\n\nuh oh! degenerate solution')
         print('payoffs are\n', payoff)
@@ -126,19 +127,34 @@ class Protagonist:
     
     def get_curr_policy(self):
         '''
-        return policy of strategy
+        sample a policy from strategy
         '''
+        sample_i = sample_strategy(self.strategy)
+        sampled_policy = self.get_policy_i(sample_i)
+        print(f"--- sample prog policy: {sample_i}")
+
+        # copy参数，创建另一个instance
         curr_policy = self.get_a_policy()
-        worker_state_dict = [x.state_dict() for x in self.policies]
-        weight_keys = list(worker_state_dict[0].keys())
+        # worker_state_dict = [x.state_dict() for x in self.policies]
+        state_dict = sampled_policy.state_dict()
+        weight_keys = list(state_dict.keys())
         fed_state_dict = collections.OrderedDict()
         for key in weight_keys:
-            key_sum = 0
-            for i in range(self.strategy_length):
-                key_sum = key_sum + worker_state_dict[i][key] * self.strategy[i]
-            fed_state_dict[key] = key_sum
-        #### update fed weights to fl model
+            fed_state_dict[key] = state_dict[key]
+        # #### update fed weights to fl model
         curr_policy.load_state_dict(fed_state_dict)
+
+        # curr_policy = self.get_a_policy()
+        # worker_state_dict = [x.state_dict() for x in self.policies]
+        # weight_keys = list(worker_state_dict[0].keys())
+        # fed_state_dict = collections.OrderedDict()
+        # for key in weight_keys:
+        #     key_sum = 0
+        #     for i in range(self.strategy_length):
+        #         key_sum = key_sum + worker_state_dict[i][key] * self.strategy[i]
+        #     fed_state_dict[key] = key_sum
+        # #### update fed weights to fl model
+        # curr_policy.load_state_dict(fed_state_dict)
         # print("get curr policy done!")
         return curr_policy
     
@@ -156,58 +172,66 @@ class Protagonist:
             strategy = strategy.tolist()
         self.strategy = strategy
     
-    def get_best_response(self, adversary, cfg, callbacks, logger, init=False):
+    def get_best_response(self, adversary, cfg, callbacks, logger, epoch, init=False):
         '''
         fix adversary and update Protagonist
         '''
         print("===== in protagonist bs ====")
         
+        max_epoch = 3
+        # if epoch == 0:
+        #     max_epoch = 20
+        # elif epoch > 0 and epoch < 5:
+        #     max_epoch = 15
+        # else: 
+        #     max_epoch = 15
         
-        log.info("Instantiating trainer...")
-        trainer: RL4COTrainer = hydra.utils.instantiate(
-            cfg.trainer,
-            max_epochs=25,
-            callbacks=callbacks,
-            logger=logger,
-        )
-        # get protagonist's policy from strategy: params add
-        cur_policy = self.get_curr_policy()     # AttentionModel's policy
-        log.info(f"Instantiating protagonist model <{cfg.model._target_}>")
+        # get protagonist's policy from strategy: adver用策略变化，prog更新policy
+        cur_policy = self.get_curr_policy()     # sample a AttentionModel's policy
+        # log.info(f"Instantiating protagonist model <{cfg.model._target_}>")
         cur_model: LightningModule = hydra.utils.instantiate(cfg.model, self.env, policy=cur_policy)
-    
-        # get adver's policy from its' strategy: params add
-        adver_curr_policy, adver_curr_critic = adversary.get_curr_policy()
-        log.info(f"Instantiating adversary model <{cfg.model_adversary._target_}>")
-        adver_cur_model: LightningModule = hydra.utils.instantiate(cfg.model_adversary, self.env, 
-                                                             policy=adver_curr_policy, critic=adver_curr_critic)
 
+        # get adver's policy from its' strategy: 
+        adver_curr_policy, adver_curr_critic = adversary.get_curr_policy()
+        # log.info(f"Instantiating adversary model <{cfg.model_adversary._target_}>")
+        adver_cur_model: LightningModule = hydra.utils.instantiate(cfg.model_adversary, self.env, 
+                                                            policy=adver_curr_policy, critic=adver_curr_critic)
 
         # fix adver's params / not update
         # run until can't get more reward / reward converge
         psro_model: LightningModule = hydra.utils.instantiate(cfg.model_psro, self.env, cur_model, adver_cur_model, 
-                                                              fix_protagonist=False,
-                                                              fix_adversary=True)
+                                                            fix_protagonist=False,
+                                                            fix_adversary=True)
+        for _ in range(max_epoch):
+        # log.info("Instantiating trainer...")
+            trainer: RL4COTrainer = hydra.utils.instantiate(
+                cfg.trainer,
+                max_epochs=1,
+                callbacks=callbacks,
+                logger=logger,
+            )
 
-        
-
-        if cfg.get("train"):
-            log.info("Starting training!")
-            if init:
-                if cfg.load_prog_from_path:
-                    cur_model = cur_model.load_from_checkpoint(cfg.load_prog_from_path)
-                    cur_policy = cur_model.policy
-                    print("load psro pretrained")
+            if cfg.get("train"):
+                log.info("Starting training!")
+                if init:
+                    if cfg.load_prog_from_path:
+                        cur_model = cur_model.load_from_checkpoint(cfg.load_prog_from_path)
+                        cur_policy = cur_model.policy
+                        print("load psro pretrained")
+                    else:
+                        trainer.fit(model=psro_model, ckpt_path=cfg.get("ckpt_path"))
                 else:
                     trainer.fit(model=psro_model, ckpt_path=cfg.get("ckpt_path"))
-                    cur_policy = psro_model.protagonist.policy
-            else:
-                trainer.fit(model=psro_model, ckpt_path=cfg.get("ckpt_path"))
-                cur_policy = psro_model.protagonist.policy
-            # 取训练完的val reward（最后一次）
-            curr_reward = psro_model.last_val_reward.to("cpu")
-            # print("wait")
+                # 取训练完的val reward（最后一次）
+                curr_reward = psro_model.last_val_reward.to("cpu")
+                # print("wait")
 
-        return cur_policy, curr_reward
+            # 每次重新采样adver
+            adver_tmp_policy, adver_tmp_critic = adversary.get_curr_policy()
+            adver_cur_model.policy = adver_tmp_policy
+            adver_cur_model.critic = adver_tmp_critic
+
+        return psro_model.protagonist.policy, curr_reward
 
 class Adversary:
     def __init__(self, model, policy, critic, env) -> None:
@@ -237,31 +261,40 @@ class Adversary:
     
     def get_curr_policy(self):
         '''
-        return policy of strategy
+        sample a policy from strategy
         '''
+        sample_i = sample_strategy(self.strategy)
+        sampled_policy, sampled_critic = self.get_policy_i(sample_i)
+        print(f"--- sample afversary policy: {sample_i}")
         curr_policy, curr_critic = self.get_a_policy()
+        # worker_state_dict = [x.state_dict() for x in self.policies]
+        state_dict_policy = sampled_policy.state_dict()
+        weight_keys_policy = list(state_dict_policy.keys())
+        fed_state_dict_policy = collections.OrderedDict()
 
-        worker_state_dict = [x.state_dict() for x in self.policies]
-        weight_keys = list(worker_state_dict[0].keys())
-        fed_state_dict = collections.OrderedDict()
-        for key in weight_keys:
-            key_sum = 0
-            for i in range(self.strategy_length):     # 在best response后，strategy维度还没有改变。
-                key_sum = key_sum + worker_state_dict[i][key] * self.strategy[i]
-            fed_state_dict[key] = key_sum
-        #### update fed weights to fl model
-        curr_policy.load_state_dict(fed_state_dict)
+        for key in weight_keys_policy:
+            fed_state_dict_policy[key] = state_dict_policy[key]
+        curr_policy.load_state_dict(fed_state_dict_policy)
 
-        worker_state_dict = [x.state_dict() for x in self.correspond_critic]
-        weight_keys = list(worker_state_dict[0].keys())
-        fed_state_dict = collections.OrderedDict()
-        for key in weight_keys:
-            key_sum = 0
-            for i in range(self.strategy_length):
-                key_sum = key_sum + worker_state_dict[i][key] * self.strategy[i]
-            fed_state_dict[key] = key_sum
-        #### update fed weights to fl model
-        curr_critic.load_state_dict(fed_state_dict)
+        state_dict_critic = sampled_critic.state_dict()
+        weight_keys_critic = list(state_dict_critic.keys())
+        fed_state_dict_critic = collections.OrderedDict()
+        for key in weight_keys_critic:
+            fed_state_dict_critic[key] = state_dict_critic[key]
+        curr_critic.load_state_dict(fed_state_dict_critic)
+        # #### update fed weights to fl model
+        # curr_policy.load_state_dict(fed_state_dict)
+
+        # worker_state_dict = [x.state_dict() for x in self.correspond_critic]
+        # weight_keys = list(worker_state_dict[0].keys())
+        # fed_state_dict = collections.OrderedDict()
+        # for key in weight_keys:
+        #     key_sum = 0
+        #     for i in range(self.strategy_length):
+        #         key_sum = key_sum + worker_state_dict[i][key] * self.strategy[i]
+        #     fed_state_dict[key] = key_sum
+        # #### update fed weights to fl model
+        # curr_critic.load_state_dict(fed_state_dict)
         return curr_policy, curr_critic
     
     @property
@@ -279,6 +312,7 @@ class Adversary:
             strategy = strategy.tolist()
         self.strategy = strategy
     
+    
     def get_best_response(self, protagonist, cfg, callbacks, logger):
         '''
         fix Protagonist and update adversary
@@ -286,17 +320,12 @@ class Adversary:
         print("===== in adversary bs ====")
         
 
-        log.info("Instantiating trainer...")
-        trainer: RL4COTrainer = hydra.utils.instantiate(
-            cfg.trainer,
-            max_epochs=2,
-            callbacks=callbacks,
-            logger=logger,
-        )
+        
         # get protagonist's policy from strategy: params add
         # get adver's policy from its' strategy: params add
-        # fix adver's params / not update
-        # run until can't get more reward / reward converge
+        
+        
+
         # get protagonist's policy from strategy: params add
         prog_policy = protagonist.get_curr_policy()
         log.info(f"Instantiating protagonist model <{cfg.model._target_}>")
@@ -316,18 +345,32 @@ class Adversary:
                                                               fix_protagonist=True,
                                                               fix_adversary=False)
 
+        for _ in range(2):
+            
+            if cfg.get("train"):
+                # log.info("Instantiating trainer...")
+                trainer: RL4COTrainer = hydra.utils.instantiate(
+                        cfg.trainer,
+                        max_epochs=1,
+                        callbacks=callbacks,
+                        logger=logger,
+                    )
+                
+                # log.info("Starting training!")
+                
+                trainer.fit(model=psro_model, ckpt_path=cfg.get("ckpt_path"))
+                curr_reward = psro_model.last_val_reward.to("cpu")
+            
+            prog_tmp_policy = protagonist.get_curr_policy()
+            prog_model.policy = prog_tmp_policy
 
-        
-        if cfg.get("train"):
-            log.info("Starting training!")
-            # if cfg.train_with_pretrain:
-            #     model = model.load_from_checkpoint(cfg.train_with_pretrain)
-            #     print("load psro pretrained")
-            # else:
-            #     print("no load psro pretrained")
-            trainer.fit(model=psro_model, ckpt_path=cfg.get("ckpt_path"))
-            curr_reward = psro_model.last_val_reward.to("cpu")
         return cur_policy, cur_critic, curr_reward
+
+def sample_strategy(distrib):
+    strategy_is = random.choices(list(range(len(distrib))), weights=distrib)
+    print("in func", strategy_is)
+    strategy_i = strategy_is[0]
+    return strategy_i
 
 @hydra.main(version_base="1.3", config_path="../configs", config_name="main_psro_frame.yaml")
 def run_psro(cfg: DictConfig):
@@ -335,13 +378,13 @@ def run_psro(cfg: DictConfig):
     # trainer.logger = logger
     log.info("Instantiating callbacks...")
     callbacks: List[Callback] = utils.instantiate_callbacks(cfg.get("callbacks"))
-    early_stopping = EarlyStopping(monitor='val/reward', min_delta=0.00, patience=10, mode="max")      # max代表曲线上升，阈值达到-7.99; 否则默认min
-    callbacks.append(early_stopping)  
+    # early_stopping = EarlyStopping(monitor='val/reward', min_delta=0.00, patience=10, mode="max")      # max代表曲线上升，阈值达到-7.99; 否则默认min
+    # callbacks.append(early_stopping)  
 
     log.info("Instantiating loggers...")
     logger: List[Logger] = utils.instantiate_loggers(cfg.get("logger"))
 
-    epochs = 20
+    epochs = 15
     epsilon = 0.01      # prog, adver的bs差距阈值，小于判断为 均衡
     log.info(f"Instantiating environment <{cfg.env._target_}>")
     env = hydra.utils.instantiate(cfg.env)
@@ -356,7 +399,6 @@ def run_psro(cfg: DictConfig):
     # 分别训练两个agent的初始policy
     protagonist.strategy = [1.]
     adversary.strategy = [1.]
-    
     
     
     ## 计算初始payoff矩阵: row-prota, col-adver
@@ -389,9 +431,10 @@ def run_psro(cfg: DictConfig):
 
     print("init payoff:", payoff_prot)
     print(protagonist.policy_number)
-    for _ in range(epochs):
+    for e in range(epochs):
 
-        bs_adversary, prog_bs_reward = protagonist.get_best_response(adversary, cfg, callbacks, logger)
+        
+        bs_adversary, prog_bs_reward = protagonist.get_best_response(adversary, cfg, callbacks, logger, epoch=e)
         protagonist.add_policy(bs_adversary)
 
         bs_protagonist, bs_protagonist_critic, adver_bs_reward = adversary.get_best_response(protagonist, cfg, callbacks, logger)
@@ -399,7 +442,7 @@ def run_psro(cfg: DictConfig):
         
         # 判断是否达到平衡
         if abs(prog_bs_reward - adver_bs_reward) < epsilon:
-            print("get equalibium")
+            print(f"get equalibium in {e} epoch !!! prog reward:{prog_bs_reward}, adver reward:{adver_bs_reward}")
             break
         # 更新新加入policy的payoff矩阵
         row_range = [protagonist.policy_number - 1]
