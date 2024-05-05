203b5e3d1bb3f94f4937e1b6830f74b07ea8f366
yuanxuanS <panpanono@163.com>
2024-05-05 18:54:21 +0800

    add last

--- rl4co/tasks/eval_heuristic.py
+++ rl4co/tasks/eval_heuristic.py
@@ -67,3 +67,69 @@ def evaluate_baseline(
         np.savez(save_fname, **retvals)
     print(f"mean reward is {retvals['mean reward']}, time is {retvals['time']}")
     return retvals
+
+
+
+def evaluate_baseline_withpsroadv(
+    env,
+    dataset_filename,
+    baseline="cw",
+    adver=None,
+    save_results=True,
+    save_fname="results.npz",
+    **kwargs,
+):
+    num_loc = getattr(env, "num_loc", None)
+
+    baselines_mapping = {
+        "svrp": {
+            "cw": {"func": CW_svrp, "kwargs": {}},
+            "tabu": {
+                "func": TabuSearch_svrp, "kwargs": {}},
+            "random": {
+                "func": Random_svrp, "kwargs": {}
+            },},
+        "csp": {
+            "LS1": {"func": LocalSearch1_csp, "kwargs": {}},
+            "LS2": {"func": LocalSearch2_csp, "kwargs": {}},
+        },
+        "opsa": {
+            "LS":  {"func": localsearch_opsa, "kwargs": {}},
+        }
+        
+    }
+
+    assert baseline in baselines_mapping[env.name], "baseline {} not found".format(baseline)
+
+
+    # env td  data
+    f = getattr(env, f"test_file") if dataset_filename is None else dataset_filename
+    td_load = env.load_data(f)       # this func normalize to [0-1]
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # reset td, use this data-key-value to process in heuristic func
+    td_load = env._reset(td_load).to(device)
+
+    # adver
+    out_adv = adver(td_load.clone(), phase="test", return_actions=True)
+    td = env.reset_stochastic_var(td_load, out_adv["action_adv"][..., None])
+    
+    # heuristic is faster in cpu than GPU
+    td = td.to("cpu")
+    # Set up the evaluation function
+    eval_settings = baselines_mapping[env.name][baseline]
+    func, kwargs_ = eval_settings["func"], eval_settings["kwargs"]
+    # subsitute kwargs with the ones passed in
+    kwargs_.update(kwargs)
+    kwargs = kwargs_
+    eval_fn = func(td)
+
+
+    # Run evaluation
+    retvals = eval_fn.forward()
+
+    # Save results
+    if save_results:
+        print("Saving results to {}".format(save_fname))
+        np.savez(save_fname, **retvals)
+    print(f"mean reward is {retvals['mean reward']}, time is {retvals['time']}")
+    return retvals
